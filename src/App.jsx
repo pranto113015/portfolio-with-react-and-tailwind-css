ebab33056a48e239869fc03d01f28fa78264a728
pranto113015 <pranto3876@gmail.com>
2024-09-13 16:14:58 +0600

    creating home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
       <Navbar />
+      <Home />
     </div>
   );
 }
