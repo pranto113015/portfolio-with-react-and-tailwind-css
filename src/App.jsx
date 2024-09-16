d649e3a7cd0aef6d1864e6bf51f17a37f55708aa
pranto113015 <pranto3876@gmail.com>
2024-09-16 21:36:51 +0600

    Designing the about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./Components/About";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
       <Navbar />
       <Home />
+      <About/>
     </div>
   );
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import AboutImg from "../assets/about.png";
+import { IoArrowForward } from "react-icons/io5";
+
+const About = () => {
+  return (
+    <div className="text-white md:flex overflow-hidden items-center md:flex-wrap md:justify-center bg-black shadow-xl mx-0 md:mx-20 bg-opacity-30 rounded-lg p-12">
+      <div>
+        <h2 className="text-2xl md:text-4xl font-bold">About</h2>
+        <div className="md:flex flex-wrap flex-col md:flex-row items-center">
+          <img src={AboutImg} alt="aboutImage" />
+
+          <ul>
+            <div className="flex gap-3 py-4">
+              <IoArrowForward size={30} className="mt-1" />
+
+              <span className="w-96">
+                <h1 className="text-xl md:text-2xl font-semibold leading-normal">
+                  Frontend development
+                </h1>
+                <p className="text-sm md:text-md leading-tight">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                  quas, cum id eum quisquam eius ullam culpa delectus eos alias.
+                </p>
+              </span>
+            </div>
+
+            <div className="flex gap-3 py-4">
+              <IoArrowForward size={30} className="mt-1" />
+
+              <span className="w-96">
+                <h1 className="text-xl md:text-2xl font-semibold leading-normal">
+                  Backend development
+                </h1>
+                <p className="text-sm md:text-md leading-tight">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                  quas, cum id eum quisquam eius ullam culpa delectus eos alias.
+                </p>
+              </span>
+            </div>
+
+            <div className="flex gap-3 py-4">
+              <IoArrowForward size={30} className="mt-1" />
+
+              <span className="w-96">
+                <h1 className="text-xl md:text-2xl font-semibold leading-normal">
+                  Full Stack development
+                </h1>
+                <p className="text-sm md:text-md leading-tight">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                  quas, cum id eum quisquam eius ullam culpa delectus eos alias.
+                </p>
+              </span>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
