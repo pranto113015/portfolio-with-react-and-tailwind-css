f6854fb11c74d252300b50f4fc75861a482a52f3
pranto113015 <pranto3876@gmail.com>
2024-09-17 13:54:56 +0600

    Showcasing Your Experience

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
 
 const Navbar = () => {
   const [menu, openMenu] = useState(false);
-  const[showMenu, setShowMenu] = useState(true);
+  const[showMenu, setShowmenu] = useState(true);
   return (
     <nav className="flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20">
       <span className="text-xl font-bold tracking-wide">Portfolio</span>
@@ -42,7 +42,7 @@ const Navbar = () => {
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
           onClick={() => {
             openMenu(!menu);
-            setShowMenu(!showMenu);
+            setShowmenu(!showMenu);
           }}
         />
       ) : (
